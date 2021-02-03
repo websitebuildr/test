8291952be9c8b060d2b13bb135d90e8a28588247
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-03 23:36:34 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCAWVF2xvzCBWQH3WHKNj2-XoD1Joa0IyA",
+    authDomain: "havenlands-feedback-data.firebaseapp.com",
+    projectId: "havenlands-feedback-data",
+    storageBucket: "havenlands-feedback-data.appspot.com",
+    messagingSenderId: "970848745258",
+    appId: "1:970848745258:web:4b5b7bd6f1e43de9459053"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
