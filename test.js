403af0652dc673ef270a3e0442fa23778e76fe6a
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-08 14:06:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCAWVF2xvzCBWQH3WHKNj2-XoD1Joa0IyA",
-    authDomain: "havenlands-feedback-data.firebaseapp.com",
-    projectId: "havenlands-feedback-data",
-    storageBucket: "havenlands-feedback-data.appspot.com",
-    messagingSenderId: "970848745258",
-    appId: "1:970848745258:web:4b5b7bd6f1e43de9459053"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+        apiKey: 'AIzaSyCAWVF2xvzCBWQH3WHKNj2-XoD1Joa0IyA',
+        authDomain: 'havenlands-feedback-data.firebaseapp.com',
+        projectId: 'havenlands-feedback-data'
+      });
+
+      const db = firebase.firestore();  
