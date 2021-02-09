ebfb390e60841418b4c1dce214e24e8429804f28
websitebuildr <78500863+websitebuildr@users.noreply.github.com>
2021-02-09 14:43:54 +0530

    Update function_test.js

--- function_test.js
+++ function_test.js
@@ -15,17 +15,18 @@ function sendToDiscord() {
 // upload data to cloud
 function storeData() {
     var id = String(document.getElementById('id').value)
-    db.collection('Feedbacks').doc(id).set({
-        // id:id,
+    db.collection('Feedbacks').add({
+        id:id,
         name:document.getElementById('name').value,
-        msg:document.getElementById('msg').value
+        ingamename:document.getElementById('in-game-name').value,
+        msg:document.getElementById('message').value
     })
     .then((doc) => {
         console.log("Document written with ID: ", doc.id);
+        // alert("Message has been sent to our staff with your Discord ID : ", doc.id)
     })
     .catch((error) => {
         console.error("Error adding document: ", error);
     });
 
 }
-
